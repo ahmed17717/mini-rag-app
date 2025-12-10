c2995cd6ceeedf3747f8f5784cd1727046a9ddf5
ahmed <ahmed6532256@gmail.com>
2025-12-10 17:42:12 +0200

    Remove env files and clean history

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter, Depends
-from helpers.config import get_settings, settings
+from helpers.config import get_settings, Settings
 
 
 base_router = APIRouter(
@@ -8,7 +8,7 @@ base_router = APIRouter(
 )
 
 @base_router.get('/')
-async def welcome(app_settings: settings = Depends(get_settings)):
+async def welcome(app_settings: Settings = Depends(get_settings)):
   app_name = app_settings.APP_NAME
   app_version = app_settings.APP_VERSION
 
